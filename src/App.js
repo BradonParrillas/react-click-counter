fd1caa50378adf404a01271b4790f84ddb72a16c
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-15 13:45:01 -0600

    import logo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import logo from "./images/logo.png"
 
 function App() {
   return (
     <div className="App">
+      <div className="container-logo">
+        <img
+          className="logo"
+          src={logo}
+          alt="logo" 
+        />
+      </div>
     </div>
   );
 }
