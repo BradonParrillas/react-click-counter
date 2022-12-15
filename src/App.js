6e7ff3db8e477905102dc4c8cdca415eaec31b1b
Daniel Parrillas <daniel16parrillas@gmail.com>
2022-12-15 13:59:17 -0600

    styles & html structure

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
-import logo from "./images/logo.png"
+import "./App.css";
+import logo from "./images/logo.png";
 
 function App() {
   return (
     <div className="App">
       <div className="container-logo">
-        <img
-          className="logo"
-          src={logo}
-          alt="logo" 
-        />
+        <img className="logo" src={logo} alt="logo" />
       </div>
+      <div className="cointainer-main"></div>
     </div>
   );
 }
